ede31529b6d36ac2873cba703b9dc2a3ed116f5d
boilercoding <boilercoding@gmail.com>
2017-07-03 23:27:06 -0400

    Adds registration changeset to user

--- web/models/user.ex
+++ web/models/user.ex
@@ -24,4 +24,24 @@ defmodule Crm.User do
     |> validate_format(:username, ~r/^[a-zA-Z0-9_.-]*$/, message: "Please use letters and numbers without space(only characters allowed _ . -)")
     |> unique_constraint(:username)
   end
+
+  def reg_changeset(struct, params \\ %{}) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 8, max: 100)
+    |> validate_format(:password, ~r/^[a-zA-Z0-9_.-]*$/, message: "Please use letters and numbers without space(only characters allowed _ . -)")
+    |> validate_format(:password, ~r/^(?=.*[a-zA-Z])(?=.*[0-9])/, message: "Password must contain letters and numbers")
+    |> validate_confirmation(:password)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Hasher.salted_password_hash(pass))
+      _ ->
+        changeset
+    end
+  end
 end
