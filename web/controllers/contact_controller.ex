4b036f945bee575ac035af33575501d23a9c6514
boilercoding <boilercoding@gmail.com>
2017-07-04 04:53:17 -0400

    Adds create for contacts

--- web/controllers/contact_controller.ex
+++ web/controllers/contact_controller.ex
@@ -2,10 +2,9 @@ defmodule Crm.ContactController do
   use Crm.Web, :controller
   use Drab.Controller
 
-  alias Crm.Contact
-  alias Crm.ContactGroup
+  alias Crm.{Contact, ContactGroup}
 
-  plug :assign_user_id_to_session when not action in [:new]
+  plug :assign_user_id_to_session when action in [:new]
 
   def index(conn, _params) do
     render conn, :index
@@ -17,10 +16,27 @@ defmodule Crm.ContactController do
     render conn, :new, changeset: changeset, groups: groups
   end
 
+  def create(conn, %{"contact" => contact_params}) do
+    groups = Repo.all(ContactGroup)
+    group = String.to_integer(contact_params["contact_group_id"])
+    changeset = Contact.changeset(%Contact{
+      contact_group_id: group,
+      user_id: conn.assigns.current_user.id
+    }, contact_params)
+
+    case Repo.insert(changeset) do
+      {:ok, contact} ->
+        conn
+        |> put_flash(:info, "#{contact.name} created!")
+        |> redirect(to: contact_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, :new, changeset: changeset, groups: groups)
+    end
+  end
+
   defp assign_user_id_to_session(conn, _) do
-    current_user = conn.current_user
     if conn.assigns.current_user do
-      conn = put_session(conn, :user_id, current_user.id)
+      conn = put_session(conn, :user_id, conn.assigns.current_user.id)
     end
     conn
   end
