6d11171defb32f364c3a3992510e27603a68408f
boilercoding <boilercoding@gmail.com>
2017-07-18 04:30:11 -0400

    Adds deployment configs

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule Crm.UserSocket do
   # channel "room:*", Crm.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
